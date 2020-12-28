053d6368dc67ec25ae733c47e545704cc364c0db
kkumarasamy <kkumarasamy@edit-place.com>
2020-12-28 22:11:36 +0530

    pakage moment replaced with date-fns

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,24 +1,32 @@
 /* eslint-disable */
-import Vue from 'vue'
-import moment from 'moment'
+import Vue from "vue";
+// import moment from "moment";
+import { format, parseISO } from "date-fns";
 
-Vue.filter('shortenText', function(text, maxLength = 300) {
-    // debugger
-    if(text && typeof(text) === 'string') {
-        const finalChar = text.length > maxLength ? '...' : ''
-        return text.substr(0, maxLength) + finalChar
-    }
+Vue.filter("shortenText", function(text, maxLength = 300) {
+  // debugger
+  if (text && typeof text === "string") {
+    const finalChar = text.length > maxLength ? "..." : "";
+    return text.substr(0, maxLength) + finalChar;
+  }
 
-    return ''
-})
+  return "";
+});
 
-Vue.filter('capitalize', function(string) {
-    // debugger
-    return string.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())))
-})
+Vue.filter("capitalize", function(string) {
+  // debugger
+  return string.replace(/\w\S*/g, w => w.replace(/^\w/, c => c.toUpperCase()));
+});
 
-Vue.filter('formatDate', function(date, dateFormat = 'LLL') {
-    // debugger
-    if(!date) return ''
-    return moment(date).format(dateFormat)
-})
+/* Vue.filter("formatDate2", function(date, dateFormat = "LLL") {
+  // debugger
+  if (!date) return "";
+  return moment(date).format(dateFormat);
+}); */
+
+// replaced moment with dat-fns
+Vue.filter("formatDate", function(date, dateFormat = "LLLL dd, yyyy hh:mm aa") {
+  // debugger
+  if (!date) return "";
+  return format(parseISO(date), dateFormat);
+});
--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,128 +1,139 @@
-const router = require('express').Router()
-const moment = require('moment')
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-const verifyToken = require('../middlewares/verify-token')
-const User = require('../models/user')
-const Order = require('../models/order')
+const router = require("express").Router();
+// const moment = require("moment");
+import { add, format } from "date-fns";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const verifyToken = require("../middlewares/verify-token");
+const User = require("../models/user");
+const Order = require("../models/order");
 
 const SHIPMENT = {
-    normal: {
-        price: 13.98,
-        days: 7
-    },
-    fast: {
-        price: 49.98,
-        days: 3
-    },
-}
+  normal: {
+    price: 13.98,
+    days: 7
+  },
+  fast: {
+    price: 49.98,
+    days: 3
+  }
+};
 
 function shipmentPrice(shipmentOption) {
-    let estimated = moment().add(shipmentOption.days, 'd').format('dddd MMM Do')
+  // debugger;
+  /* let estimated2 = moment()
+    .add(shipmentOption.days, "d")
+    .format("dddd MMM Do"); */
+  let estimated = format(
+    add(new Date(), {
+      days: shipmentOption.days
+    }),
+    "LLLL dd, yyyy hh:mm aa"
+  );
 
-    return {
-        estimated,
-        price: shipmentOption.price
-    }
+  return {
+    estimated,
+    price: shipmentOption.price
+  };
 }
 
 // POST request - for shipment setting
-router.post('/shipment', (req, res) => {
-    let shipment
-    if(req.body.shipment === 'normal') {
-        shipment = shipmentPrice(SHIPMENT.normal)
-    } else {
-        shipment = shipmentPrice(SHIPMENT.fast)
-    }
+router.post("/shipment", (req, res) => {
+  let shipment;
+  if (req.body.shipment === "normal") {
+    shipment = shipmentPrice(SHIPMENT.normal);
+  } else {
+    shipment = shipmentPrice(SHIPMENT.fast);
+  }
 
-    res.json({
-        success: true,
-        shipment: shipment
-    })
-})
+  res.json({
+    success: true,
+    shipment: shipment
+  });
+});
 
 // Stripe Payment
-router.post('/payment', verifyToken, async (req, res) => {
-    let totalPrice = Math.round(req.body.totalPrice * 100)
-    let foundUser = await User.findOne({ _id: req.decoded._id }).populate('address').exec()
-    debugger
-    // console.log(foundUser)
-    stripe.customers.create({
-        email: req.decoded.email,
-        name: req.decoded.name,
-        // address: {
-        //     line1: '510 Townsend St',
-        //     postal_code: '98140',
-        //     city: 'San Francisco',
-        //     state: 'CA',
-        //     country: 'US',
-        //   },
-        address: {
-            country: foundUser.address.country,
-            line1: foundUser.address.streetAddress,
-            city: foundUser.address.city,
-            state: foundUser.address.state,
-            postal_code: foundUser.address.zipCode,
-        }
+router.post("/payment", verifyToken, async (req, res) => {
+  let totalPrice = Math.round(req.body.totalPrice * 100);
+  let foundUser = await User.findOne({ _id: req.decoded._id })
+    .populate("address")
+    .exec();
+  debugger;
+  // console.log(foundUser)
+  stripe.customers
+    .create({
+      email: req.decoded.email,
+      name: req.decoded.name,
+      // address: {
+      //     line1: '510 Townsend St',
+      //     postal_code: '98140',
+      //     city: 'San Francisco',
+      //     state: 'CA',
+      //     country: 'US',
+      //   },
+      address: {
+        country: foundUser.address.country,
+        line1: foundUser.address.streetAddress,
+        city: foundUser.address.city,
+        state: foundUser.address.state,
+        postal_code: foundUser.address.zipCode
+      }
     })
     .then(customer => {
-        return stripe.customers.createSource(customer.id, {
-            source: 'tok_visa'
-        })
+      return stripe.customers.createSource(customer.id, {
+        source: "tok_visa"
+      });
     })
     .then(source => {
-        return stripe.charges.create({
-            amount: totalPrice,
-            currency: 'inr',
-            customer: source.customer,
-            description: 'Software development services',
-        })
+      return stripe.charges.create({
+        amount: totalPrice,
+        currency: "inr",
+        customer: source.customer,
+        description: "Software development services"
+      });
     })
     .then(async charge => {
-        let order =new Order()
-        let cart = req.body.cart
+      let order = new Order();
+      let cart = req.body.cart;
 
-        cart.map(product => {
-            order.products.push({
-                productID: product._id,
-                quantity: parseInt(product.quantity),
-                price: product.price
-            })
-        })
+      cart.map(product => {
+        order.products.push({
+          productID: product._id,
+          quantity: parseInt(product.quantity),
+          price: product.price
+        });
+      });
 
-        order.owner = req.decoded._id
-        order.estimatedDelivery = req.body.estimatedDelivery
-        await order.save()
+      order.owner = req.decoded._id;
+      order.estimatedDelivery = req.body.estimatedDelivery;
+      await order.save();
 
-        res.json({
-            success: true,
-            message: 'Successfully made a payment'
-        })
+      res.json({
+        success: true,
+        message: "Successfully made a payment"
+      });
     })
     .catch(err => {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        })
-    })
-
-
-})
+      res.status(500).json({
+        success: false,
+        message: err.message
+      });
+    });
+});
 
 // GET request - get categories
-router.get('/categories', async(req,res) => {
-    try {
-        let categories = await Category.find()
-        res.json({
-            success: true,
-            categories: categories
-        })
-    } catch(err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        })
-    }
-})
+router.get("/categories", async (req, res) => {
+  try {
+    let categories = await Category.find();
+    res.json({
+      success: true,
+      categories: categories
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
 
 // GET request - get a single product
 
@@ -130,4 +141,4 @@ router.get('/categories', async(req,res) => {
 
 // DELETE request - delete a single product
 
-module.exports = router
+module.exports = router;
