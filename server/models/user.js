9f84cacc02fe7690711372a5a61b69c700841420
kkumarasamy <kkumarasamy@edit-place.com>
2021-01-01 17:19:22 +0530

    removed old bcrypt version

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require("bcrypt-nodejs");
+// const bcrypt = require("bcrypt-nodejs");
 const { genSalt, hash, compareSync } = require("bcrypt");
 
 const UserSchema = new Schema({
