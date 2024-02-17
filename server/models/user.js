0e209e39f4da8d70755b8cd09b8aa5a49fbae32c
Kathiravan K <kathirr007@gmail.com>
2024-02-17 14:11:23 +0530

    removed bcrypt and added bcryptjs instead

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // const bcrypt = require("bcrypt-nodejs");
-const { genSalt, hash, compareSync } = require("bcrypt");
+// const { genSalt, hash, compareSync } = require("bcrypt");
+const { genSalt, hash, compareSync } = require("bcryptjs");
 
 const UserSchema = new Schema({
   name: String,
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   address: { type: Schema.Types.ObjectId, ref: "Address" },
-  admin: { type: Boolean, default: false }
+  admin: { type: Boolean, default: false },
 });
 
 /* UserSchema.pre("save", function(next) {
@@ -32,15 +33,15 @@ const UserSchema = new Schema({
   }
 }); */
 
-UserSchema.pre("save", function(next) {
+UserSchema.pre("save", function (next) {
   const user = this;
 
-  genSalt(10, function(err, salt) {
+  genSalt(10, function (err, salt) {
     if (err) {
       return next(err);
     }
 
-    hash(user.password, salt, function(err, hash) {
+    hash(user.password, salt, function (err, hash) {
       if (err) {
         return next(err);
       }
@@ -51,7 +52,7 @@ UserSchema.pre("save", function(next) {
   });
 });
 
-UserSchema.methods.comparePassword = function(password, next) {
+UserSchema.methods.comparePassword = function (password, next) {
   let user = this;
   return compareSync(password, user.password);
 };
